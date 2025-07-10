32663965f34c3fe6551da2668ff2ac1d0ebd5254
Alexander W <alexanderwbw15@gmail.com>
2025-07-10 18:33:30 +0700

    fix import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
 'use client';
 
-import Link from 'next/link';
-import { useState } from 'react';
-import {
-  FaLinkedinIn,
-  FaGithub,
-  FaTwitter,
-  FaHome,
-  FaUser,
-  FaCode,
-  FaBriefcase,
-  FaEnvelope,
-  FaFire,
-  FaPaperPlane,
-} from 'react-icons/fa';
-import { BsDot } from 'react-icons/bs';
-import Image from 'next/image';
 import Navbar from './components/navbar';
 import Hero from './components/hero';
 import About from './components/about';
@@ -25,7 +9,6 @@ import Contact from './components/contact';
 import Footer from './components/footer';
 
 export default function Home() {
-
 
   return (
     <div className="bg-gray-900 text-gray-100 font-open-sans min-h-screen">
